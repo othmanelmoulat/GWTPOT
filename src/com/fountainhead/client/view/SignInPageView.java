b758039bbd890e642dcc83951ca2e95de751b4a5
othman El Moulat <othmanelmoulat@gmail.com>
2011-10-24 14:01:45 +0000

    minor changes

--- src/com/fountainhead/client/view/SignInPageView.java
+++ src/com/fountainhead/client/view/SignInPageView.java
@@ -1,64 +1,140 @@
+/**
+
+ *
+ * Licensed under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation. You may obtain a copy of the
+ * License at: http://www.gnu.org/copyleft/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.fountainhead.client.view;
 
 import com.fountainhead.client.presenter.SignInPagePresenter;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+public class SignInPageView extends ViewWithUiHandlers<SignInPageUiHandlers> implements
+SignInPagePresenter.MyView {
 
-public class SignInPageView extends ViewWithUiHandlers<SignInPageUiHandlers>
-		implements
-			SignInPagePresenter.MyView {
+	private static final String DEFAULT_USER_NAME = "administrator";
+	private static final String DEFAULT_PASSWORD = "administrator";
 
-	private static SignInPageView2UiBinder uiBinder = GWT
-			.create(SignInPageView2UiBinder.class);
+	private static String html = "<div>\n"
+			+ "<table align=\"center\">\n"
+			+ "  <tr>\n" + "<td>&nbsp;</td>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+			+ "  <tr>\n" + "<td>&nbsp;</td>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+			+ "  <tr>\n" + "<td>&nbsp;</td>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+			+ "  <tr>\n" + "<td>&nbsp;</td>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+			+ "  <tr>\n" + "<td>&nbsp;</td>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+			+ "  <tr>\n"
+			+ "    <td colspan=\"2\" style=\"font-weight:bold;\">Sign In </td>\n"
+			+ "  </tr>\n"
+			+ "  <tr>\n"
+			+ "    <td>User name</td>\n"
+			+ "    <td id=\"userNameFieldContainer\"></td>\n"
+			+ "  </tr>\n"
+			+ "  <tr>\n"
+			+ "    <td>Password</td>\n"
+			+ "    <td id=\"passwordFieldContainer\"></td>\n"
+			+ "  </tr>\n"
+			+ "  <tr>\n"
+			+ "    <td></td>\n"
+			+ "    <td id=\"signInButtonContainer\"></td>\n"
+			+ "  </tr>\n"
+			+ "  <tr>\n" + "<td>&nbsp;</td>\n" + "<td>&nbsp;</td>\n" + "</tr>\n"
+			+ "</table>\n"
+			+ "</div>\n"
+			+ "<div  align=\"center\" style=\"color:red;\" id=\"errorLabelContainer\">\n"
+			// + "  <tr>\n"
+			// + "    <td colspan=\"2\">Forget your password?</td>\n"
+			// + "  </tr>\n"
+			// + "  <tr>\n"
+			// +
+			// "    <td colspan=\"2\" style=\"color:red;\" id=\"errorLabelContainer\"></td>\n"
+			// + "  </tr>\n" + "</table>\n"
+			+ "</div>\n";
 
-	interface SignInPageView2UiBinder extends UiBinder<Widget, SignInPageView> {
-	}
-	private final Widget widget;
+	private final HTMLPanel panel;
+
+	private final TextBox userNameField;
+	private final PasswordTextBox passwordField;
+	private final Button signInButton;
+	private final Label errorLabel;
+
 	public SignInPageView() {
-		widget = SignInPageView.uiBinder.createAndBindUi(this);
+
+		panel = new HTMLPanel(html);
+		errorLabel = new Label();
+		userNameField = new TextBox();
+		passwordField = new PasswordTextBox();
+		signInButton = new Button("Sign in");
+
+		userNameField.setText(DEFAULT_USER_NAME);
+
+		// See FieldVerifier
+		// Passwords must contain at least 8 characters with at least one digit,
+		// one upper case letter, one lower case letter and one special symbol (�@#$%�).
+		passwordField.setText(DEFAULT_PASSWORD);
+
+		panel.add(userNameField, "userNameFieldContainer");
+		panel.add(passwordField, "passwordFieldContainer");
+		panel.add(signInButton, "signInButtonContainer");
+		panel.add(errorLabel, "errorLabelContainer");
+
+		bindCustomUiHandlers();
 	}
 
-	@UiField
-	TextBox userNameField;
-	@UiField
-	PasswordTextBox passwordField;
-	@UiField
-	Button signInButton;
-	@UiField
-	Label errorLabel;
+	protected void bindCustomUiHandlers() {
 
-	@UiHandler("signInButton")
-	void onSignInButtonClicked(ClickEvent event) {
-		if (getUiHandlers() != null) {
-			getUiHandlers().onOkButtonClicked();
-		}
+		signInButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+
+				//				if (FieldVerifier.isValidUserName(getUserName()) &&
+				//						(FieldVerifier.isValidPassword(getPassword()))) {
+				if (getUiHandlers() != null) {
+					getUiHandlers().onOkButtonClicked();
+				}
+				// }
+				//				else {
+				//					// event.cancel();
+				//					Window.alert("You must enter a valid User name and Password.");
+				//					resetAndFocus();
+				//				}
+			}
+		});
 	}
 
 	@Override
 	public Widget asWidget() {
-		// TODO Auto-generated method stub
-		return widget;
+		return panel;
 	}
 
 	@Override
 	public String getUserName() {
-		// TODO Auto-generated method stub
 		return userNameField.getText();
 	}
 
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
 		return passwordField.getText();
+	}
+
+	@Override
+	public void resetAndFocus() {
+		userNameField.setFocus(true);
+		userNameField.selectAll();
 	}
 
 	@Override
@@ -66,12 +142,4 @@ public class SignInPageView extends ViewWithUiHandlers<SignInPageUiHandlers>
 		errorLabel.setText(errorText);
 
 	}
-
-	@Override
-	public void resetAndFocus() {
-		userNameField.setFocus(true);
-		userNameField.selectAll();
-
-	}
-
 }
